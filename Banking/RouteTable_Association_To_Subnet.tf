e59f91c8fd5ec52cb79b28aebe8da7126276fc6e
pranalij722 <panujadhav722@gmail.com>
2024-10-18 13:51:14 +0530

    Update RouteTable_Association_To_Subnet.tf

--- Banking/RouteTable_Association_To_Subnet.tf
+++ Banking/RouteTable_Association_To_Subnet.tf
@@ -1,5 +1,5 @@
 # Associate Route Table with Subnet
-resource "aws_route_table_association" "Banking_Project_RTA" {
-  subnet_id      = aws_subnet.Banking_Project_Subnet.id
-  route_table_id = aws_route_table.Banking_Project_Route_Table.id
+resource "aws_route_table_association" "Final_Banking_Project_RTA" {
+  subnet_id      = aws_subnet.Final_Banking_Project_Subnet.id
+  route_table_id = aws_route_table.Final_Banking_Project_Route_Table.id
 }
