e12593f2bb2a07ae5432ab0f426f23b854bca1fd
pranalij722 <panujadhav722@gmail.com>
2024-10-18 13:52:40 +0530

    Update Route_in_RouteTable.tf

--- Banking/Route_in_RouteTable.tf
+++ Banking/Route_in_RouteTable.tf
@@ -1,6 +1,6 @@
 # Create a Route in the Route Table to allow Internet access
-resource "aws_route" "Banking_Project_Route" {
-  route_table_id         = aws_route_table.Banking_Project_Route_Table.id
+resource "aws_route" "Final_Banking_Project_Route" {
+  route_table_id         = aws_route_table.Final_Banking_Project_Route_Table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.Banking_Project_IGW.id
+  gateway_id             = aws_internet_gateway.Final_Banking_Project_IGW.id
 }
