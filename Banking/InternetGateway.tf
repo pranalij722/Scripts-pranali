176f05e74fec716488a82448f4ba0c4143677674
pranalij722 <panujadhav722@gmail.com>
2024-10-18 13:48:37 +0530

    Update InternetGateway.tf

--- Banking/InternetGateway.tf
+++ Banking/InternetGateway.tf
@@ -1,7 +1,7 @@
 # Create Internet Gateway
-resource "aws_internet_gateway" "Banking_Project_IGW" {
-  vpc_id = aws_vpc.Banking_Project_VPC.id
+resource "aws_internet_gateway" "Final_Banking_Project_IGW" {
+  vpc_id = aws_vpc.Final_Banking_Project_VPC.id
   tags = {
-    Name = "Banking-Project-InternetGateway"
+    Name = "Final_Banking-Project-InternetGateway"
   }
 }
