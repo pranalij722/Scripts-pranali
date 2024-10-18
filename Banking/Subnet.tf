53dc254150e15c41a412fa23de4344a9ce3c086d
pranalij722 <panujadhav722@gmail.com>
2024-10-18 13:56:04 +0530

    Update Subnet.tf

--- Banking/Subnet.tf
+++ Banking/Subnet.tf
@@ -1,9 +1,9 @@
 # Create Subnet
-resource "aws_subnet" "Banking_Project_Subnet" {
-  vpc_id            = aws_vpc.Banking_Project_VPC.id
+resource "aws_subnet" "Final_Banking_Project_Subnet" {
+  vpc_id            = aws_vpc.Final_Banking_Project_VPC.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
   tags = {
-    Name = "Banking-Project-Subnet"
+    Name = "Final_Banking-Project-Subnet"
   }
 }
