26648e3546f6123accdc07f39ec6f99e77f11da7
pranalij722 <panujadhav722@gmail.com>
2024-10-18 14:39:06 +0530

    Update Subnet.tf

--- Banking/Subnet.tf
+++ Banking/Subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "Final_Banking_Project_Subnet" {
   vpc_id            = aws_vpc.Final_Banking_Project_VPC.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2"
+  availability_zone = "us-east-2a"
   tags = {
     Name = "Final_Banking-Project-Subnet"
   }
