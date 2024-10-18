af5aaafe01d812955917dcfd986fc8114d820fb0
pranalij722 <panujadhav722@gmail.com>
2024-10-18 13:57:01 +0530

    Update VPC.tf

--- Banking/VPC.tf
+++ Banking/VPC.tf
@@ -1,7 +1,7 @@
 # Create VPC
-resource "aws_vpc" "Banking_Project_VPC" {
+resource "aws_vpc" "Final_Banking_Project_VPC" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "Banking-Project-VPC"
+    Name = "Final_Banking-Project-VPC"
   }
 }
